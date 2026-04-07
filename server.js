007a4f5f43c79d9e4012669dd6aa4e5534e8ee28
businesszone1919-rgb <businesszone1919@gmail.com>
2026-04-07 23:17:02 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,31 +1,17 @@
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
-const io = require('socket.io')(http, {
-  cors: { origin: "*" }
-});
+const io = require('socket.io')(http, { cors: { origin: "*" } });
 const path = require('path');
 
-// 'public' ፎልደር ውስጥ ያሉትን ፋይሎች (html, css, js) እንዲያነብ ያደርጋል
+// 'public' ፎልደርን ለ Render ማሳወቅ
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-io.on('connection', (socket) => {
-  console.log('አዲስ ተጫዋች ገብቷል');
-  
-  socket.on('numberClicked', (data) => {
-    io.emit('updateBoard', data);
-  });
-
-  socket.on('disconnect', () => {
-    console.log('ተጫዋች ወጥቷል');
-  });
-});
-
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`ሰርቨሩ በፖርት ${PORT} ላይ እየሰራ ነው`);
+http.listen(PORT, () => {
+  console.log('Server is Live on port ' + PORT);
 });
